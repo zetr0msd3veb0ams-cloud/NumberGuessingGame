5bd9868587c0c3f72b494234dddf85e06b88eb60
Zetrom <zetr0msd3veb0ams@gmail.com>
2026-01-07 00:34:35 +0100

    Add files via upload

---
+++ Number guessing game/script.js
@@ -0,0 +1,32 @@
+let numberToGuess = Math.round(Math.random()* 100); 
+
+let tries = 0;
+
+
+
+function guessTheNumber() {
+    tries = tries + 1;
+    displayTries.innerHTML = 'Versuche: ' + tries;
+
+    if(numberToGuess == myNumber.value){
+        headline.innerHTML = 'Du hast gewonnen!!! 😁😁😁';
+        let jsConfetti = new JSConfetti();
+        jsConfetti.addConfetti();
+
+    }
+
+      if(numberToGuess > myNumber.value){
+
+        headline.innerHTML = 'Die Zahl ist größer!😒';
+
+    }
+
+     if(numberToGuess < myNumber.value){
+
+        headline.innerHTML = 'Die Zahl ist kleiner!😂';
+
+    }
+
+myNumber.value = "";
+
+}
